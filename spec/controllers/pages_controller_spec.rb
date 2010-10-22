47fc7afa94a3fbdced8a4739f8d77f64b3183e0c
Samuel Lin <hsianglin124@gmail.com>
2010-10-22 14:14:41 +0800

    3rd commitment

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,6 +8,12 @@ integrate_views
       get 'home'
       response.should be_success
     end
+	
+	it "should have a title" do
+		get 'home'
+		response.should have_tag("title", "Sample test app for Rspec | Home")
+	end
+	
   end
 
   describe "GET 'contact'" do
@@ -15,12 +21,22 @@ integrate_views
       get 'contact'
       response.should be_success
     end
+	
+	it "should have a title" do
+		get 'contact'
+		response.should have_tag("title", "Sample test app for Rspec | Contact")
+	end
   end
   
   describe "GET 'about'" do
 	it "should be successful" do
 		get 'about'
 		response.should be_success
+	end
+	
+	it "should have a title" do
+		get 'about'
+		response.should have_tag("title", "Sample test app for Rspec | About")
 	end
   end
   
